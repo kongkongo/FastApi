fed0a07a36626332f89eae59f10dda674fbded2a
kongkongo <55430282+kongkongo@users.noreply.github.com>
2021-03-21 11:47:13 +0800

    Add files via upload

---
+++ LearnCode/FastApi_learn/learn/file_and_uploadfile.py
@@ -0,0 +1,38 @@
+from    typing  import  List
+from    fastapi import  FastAPI,File,UploadFile,Form
+from    fastapi.responses   import  HTMLResponse
+
+import  uvicorn 
+
+app=FastAPI()
+# @app.post("/files/")
+# async   def     create_files(files:List[bytes]=File(...)):
+#     return  {"file_sizes":[len(file) for file in files]}
+
+# @app.post("/uploadfiles/")
+# async   def     create_upload_files(files:List[UploadFile]=File(...)):
+#     return  {"filenames":[file.filename for file in files]}
+
+@app.get("/")
+async   def main():
+    content="""
+<body>
+<form action="/files/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return  HTMLResponse(content=content)
+
+@app.post("/files/")
+async   def create_file(file:bytes=File(...),fileb:UploadFile=File(...),token:str=Form(...)):
+    return  {"file_size":len(file),"token":token,"fileb_content_type":fileb.content_type}
+
+
+if  "__main__":
+        uvicorn.run(app, host="0.0.0.0", port=8000)
